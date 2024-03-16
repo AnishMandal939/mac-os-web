b37122d4f33119f8305f5977469e81d42e35d076
AnishMandal939 <mandalktaanees12@gmail.com>
2024-03-16 16:13:16 +0545

    feat - configuration and add routing for applications module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BootComponent } from './core/boot/boot.component';
+import { LoginComponent } from './core/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/boot', pathMatch: 'full'
+  },
+  { path: 'desktop', loadChildren: () => import('./applications/applications.module').then(m => m.ApplicationsModule) },
+  { path: 'boot', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: '**', redirectTo: '/boot'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
